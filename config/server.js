a59f08a2f2015f95dcd724f57b00ee3040586119
AHerod <agn.j.herod@gmail.com>
2021-04-13 22:56:48 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7e69a4a76e08f117356dc292b2bacca6'),
+    },
+  },
+});
